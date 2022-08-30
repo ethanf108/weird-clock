5eedb578436844ad9d3ba4fe6b58b6332ea9ab96
Ethan Ferguson <ethanf108@gmail.com>
2022-08-30 17:03:04 -0400

    Initial Commit
    Basic clock function

---
+++ src/weird_clock/main.cljs
@@ -0,0 +1,44 @@
+(ns weird-clock.main)
+
+
+(def hours '(1 2 3 4 5 6 7 8 9 10 11 12))
+
+(def draw-canvas (.getElementById js/document "draw"))
+
+(def draw-width (.-width draw-canvas))
+(def draw-height (.-height draw-canvas))
+(def draw-ctx (.getContext draw-canvas "2d"))
+
+(defn draw-line [ctx angle len] 
+    (.beginPath ctx)
+    (.moveTo ctx (/ draw-width 2) (/ draw-height 2))
+    (.lineTo ctx 
+        (+ (/ draw-width 2) (* (/ draw-width 2) (Math/cos angle) len))
+        (+ (/ draw-height 2) (* (/ draw-height 2) (Math/sin angle) len)))
+    (.stroke draw-ctx))
+
+
+(defn draw-fn [] 
+    (set! (.-fillStyle draw-ctx) "rgb(0,255,255)")
+    (.fillRect draw-ctx 0 0 draw-width draw-height)
+    (set! (.-font draw-ctx) "18px sans-serif")
+    (set! (.-fontStyle draw-ctx) "bold")
+    (set! (.-fillStyle draw-ctx) "rgb(0,0,0)")
+    (loop 
+        [index 11]
+        (when (>= index 0)
+            (.fillText draw-ctx
+                (nth hours index)
+                (+ (/ draw-width 2) (* (/ draw-width 2) (Math/cos (/ (* Math/PI index) 6)) 0.9))
+                (+ (/ draw-height 2) (* (/ draw-height 2) (Math/sin (/ (* Math/PI index) 6)) 0.9)))
+            (recur (- index 1))))
+    (set! (.-fillStyle draw-ctx) "rgb(255,0,0)")
+    (set! (.-lineWidth draw-ctx) 6)
+    (let [
+        time (/ (.now js/Date) 1000 60)
+        minute (mod time 60)
+        hour (- (mod (/ time 60) 12) (/ (.getTimezoneOffset (js/Date.)) 60))]
+        (draw-line draw-ctx (- (* Math/PI minute .0333333333) (/ Math/PI 2)) 0.9)
+        (draw-line draw-ctx (- (* Math/PI hour (/ 1 6)) (/ Math/PI 2)) 0.6)))
+
+(js/setInterval draw-fn 1000)
