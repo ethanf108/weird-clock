7af517b0efec6f11ad0cfaa29e6a578f27e7f0a3
Ethan Ferguson <ethanf108@gmail.com>
2022-09-02 18:57:58 -0400

    functioning weird hour hand

--- src/weird_clock/main.cljs
+++ src/weird_clock/main.cljs
@@ -1,7 +1,7 @@
 (ns weird-clock.main)
 
 
-(def hours '(1 2 3 4 5 6 7 8 9 10 11 12))
+(def hours (shuffle '(1 2 3 4 5 6 7 8 9 10 11 12)))
 
 (def draw-canvas (.getElementById js/document "draw"))
 
@@ -12,7 +12,7 @@
 (defn draw-line [ctx angle len] 
     (.beginPath ctx)
     (.moveTo ctx (/ draw-width 2) (/ draw-height 2))
-    (.lineTo ctx 
+    (.lineTo ctx
         (+ (/ draw-width 2) (* (/ draw-width 2) (Math/cos angle) len))
         (+ (/ draw-height 2) (* (/ draw-height 2) (Math/sin angle) len)))
     (.stroke draw-ctx))
@@ -37,8 +37,10 @@
     (let [
         time (/ (.now js/Date) 1000 60)
         minute (mod time 60)
-        hour (- (mod (/ time 60) 12) (/ (.getTimezoneOffset (js/Date.)) 60))]
+        hour (- (mod (/ time 60) 12) (/ (.getTimezoneOffset (js/Date.)) 60))
+        startHour (.indexOf hours (Math/floor hour))
+        endHour (.indexOf hours (Math/floor (+ hour 1)))]
         (draw-line draw-ctx (- (* Math/PI minute .0333333333) (/ Math/PI 2)) 0.9)
-        (draw-line draw-ctx (- (* Math/PI hour (/ 1 6)) (/ Math/PI 2)) 0.6)))
+        (draw-line draw-ctx (* 2 Math/PI (/ (+ startHour (* (/ minute 60) (mod (- endHour startHour) 12))) 12)) 0.6)))
 
 (js/setInterval draw-fn 1000)
