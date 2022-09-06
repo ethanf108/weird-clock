e00e84f7f571374eb9ee1813b37dc6791f8638f6
Ethan Ferguson <ethanf108@gmail.com>
2022-09-06 12:14:21 -0400

    Better size support

--- src/weird_clock/main.cljs
+++ src/weird_clock/main.cljs
@@ -5,13 +5,11 @@
 
 (def draw-canvas (.getElementById js/document "draw"))
 
-(def draw-width (.-width draw-canvas))
-(def draw-height (.-height draw-canvas))
 (def draw-ctx (.getContext draw-canvas "2d"))
 
 (def params (js/URLSearchParams. (.-search js/location)))
 
-(defn draw-line [ctx angle len] 
+(defn draw-line [ctx draw-width draw-height angle len]
     (.beginPath ctx)
     (.moveTo ctx (/ draw-width 2) (/ draw-height 2))
     (.lineTo ctx
@@ -19,43 +17,39 @@
         (+ (/ draw-height 2) (* (/ draw-height 2) (Math/sin angle) len)))
     (.stroke draw-ctx))
 
-(defn switch-fullscreen []
-    (if (= (.-className draw-canvas) "fullscreen")
-    (set! (.-className draw-canvas) "")
-    (set! (.-className draw-canvas) "fullscreen")))
-
-(.addEventListener draw-canvas "click" switch-fullscreen)
-
-(when (.has params "fullscreen") (switch-fullscreen))
-
 (set! (.-textBaseline draw-ctx) "middle")
 (set! (.-textAlign draw-ctx) "center")
 
-(defn draw-fn [] 
-    (set! (.-fillStyle draw-ctx) "rgb(0,255,255)")
-    (.fillRect draw-ctx 0 0 draw-width draw-height)
-    (set! (.-font draw-ctx) "18px sans-serif")
-    (set! (.-fontStyle draw-ctx) "bold")
-    (set! (.-fillStyle draw-ctx) "rgb(0,0,0)")
-    (loop 
-        [index 11]
-        (when (>= index 0)
-            (.fillText draw-ctx
-                (nth hours index)
-                (+ (/ draw-width 2) (* (/ draw-width 2) (Math/cos (/ (* Math/PI index) 6)) 0.8))
-                (+ (/ draw-height 2) (* (/ draw-height 2) (Math/sin (/ (* Math/PI index) 6)) 0.8)))
-            (recur (- index 1))))
-    (set! (.-fillStyle draw-ctx) "rgb(255,0,0)")
-    (set! (.-lineWidth draw-ctx) 6)
+(defn draw-fn []
     (let [
-        time (/ (.now js/Date) 1000 60)
-        minute (mod time 60)
-        hour (mod (+ (/ time 60) (- (/ (.getTimezoneOffset (js/Date.)) 60))) 12)
-        startHour (.indexOf hours (Math/floor hour))
-        endHour (.indexOf hours (Math/floor (+ hour 1)))
-        startMinute (.indexOf hours (Math/floor (/ minute 5)))
-        endMinute (.indexOf hours (Math/floor (+ (/ minute 5) 1)))]
-        (draw-line draw-ctx (* 2 Math/PI (/ (+ startMinute (* (mod (- endMinute startMinute) 12) (/ (mod minute 5) 5))) 12)) 0.7)
-        (draw-line draw-ctx (* 2 Math/PI (/ (+ startHour (* (/ minute 60) (mod (- endHour startHour) 12))) 12)) 0.5)))
+        draw-width (.-offsetWidth draw-canvas)
+        draw-height (.-offsetHeight draw-canvas)]
+        (set! (.-width draw-canvas) draw-width)
+        (set! (.-height draw-canvas) draw-height)
+        (set! (.-fillStyle draw-ctx) "rgb(0,255,255)")
+        (.fillRect draw-ctx 0 0 draw-width draw-height)
+        (set! (.-font draw-ctx) "1em sans-serif")
+        (set! (.-fontStyle draw-ctx) "bold")
+        (set! (.-fillStyle draw-ctx) "rgb(0,0,0)")
+        (loop
+            [index 11]
+            (when (>= index 0)
+                (.fillText draw-ctx
+                    (nth hours index)
+                    (+ (/ draw-width 2) (* (/ draw-width 2) (Math/cos (/ (* Math/PI index) 6)) 0.8))
+                    (+ (/ draw-height 2) (* (/ draw-height 2) (Math/sin (/ (* Math/PI index) 6)) 0.8)))
+                (recur (- index 1))))
+        (set! (.-fillStyle draw-ctx) "rgb(255,0,0)")
+        (set! (.-lineWidth draw-ctx) 6)
+        (let [
+            time (/ (.now js/Date) 1000 60)
+            minute (mod time 60)
+            hour (mod (+ (/ time 60) (- (/ (.getTimezoneOffset (js/Date.)) 60))) 12)
+            startHour (.indexOf hours (Math/floor hour))
+            endHour (.indexOf hours (Math/floor (+ hour 1)))
+            startMinute (.indexOf hours (Math/floor (/ minute 5)))
+            endMinute (.indexOf hours (Math/floor (+ (/ minute 5) 1)))]
+            (draw-line draw-ctx draw-width draw-height (* 2 Math/PI (/ (+ startMinute (* (mod (- endMinute startMinute) 12) (/ (mod minute 5) 5))) 12)) 0.7)
+            (draw-line draw-ctx draw-width draw-height (* 2 Math/PI (/ (+ startHour (* (/ minute 60) (mod (- endHour startHour) 12))) 12)) 0.5))))
 
 (js/setInterval draw-fn 1000)
