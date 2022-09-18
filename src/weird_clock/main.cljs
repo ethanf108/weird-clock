255efbac4307e28abb0b1cbe6e50f6a0de835bd7
Ethan Ferguson <ethanf108@gmail.com>
2022-09-17 21:11:31 -0400

    Revert "whodunit"
    This reverts commit 28035adb9f904be067fe1f7f88df12ed8a5942d0.

--- src/weird_clock/main.cljs
+++ src/weird_clock/main.cljs
@@ -20,11 +20,6 @@
 (set! (.-textBaseline draw-ctx) "middle")
 (set! (.-textAlign draw-ctx) "center")
 
-; challenge time
-(def challenge-hour (.floor js/Math (* 12 (.random js/Math))))
-(def challenge-minute (.floor js/Math (* 60 (.random js/Math))))
-(.log js/console "Murder Myster :)")
-
 (defn draw-fn []
     (let [
         draw-width (.-offsetWidth draw-canvas)
@@ -48,8 +43,8 @@
         (set! (.-lineWidth draw-ctx) 6)
         (let [
             time (/ (.now js/Date) 1000 60)
-            minute challenge-minute ; (mod time 60)
-            hour (+ challenge-hour (/ minute 60)) ; (mod (+ (/ time 60) (- (/ (.getTimezoneOffset (js/Date.)) 60))) 12)
+            minute (mod time 60)
+            hour (mod (+ (/ time 60) (- (/ (.getTimezoneOffset (js/Date.)) 60))) 12)
             startHour (.indexOf hours (Math/floor hour))
             endHour (.indexOf hours (Math/floor (+ hour 1)))
             startMinute (.indexOf hours (Math/floor (/ minute 5)))
@@ -57,21 +52,4 @@
             (draw-line draw-ctx draw-width draw-height (* 2 Math/PI (/ (+ startMinute (* (mod (- endMinute startMinute) 12) (/ (mod minute 5) 5))) 12)) 0.7)
             (draw-line draw-ctx draw-width draw-height (* 2 Math/PI (/ (+ startHour (* (/ minute 60) (mod (- endHour startHour) 12))) 12)) 0.5))))
 
-; (js/setInterval draw-fn 1000)
-(draw-fn)
-
-(def guess-input (.getElementById js/document "guess-text"))
-
-(defn guess-fn []
-    (let [
-        value (.-value guess-input)
-        hour (js/parseInt (.substring value 0 (.indexOf value ":")))
-        minute (js/parseInt (.substring value (+ (.indexOf value ":") 1)))]
-        (when-not (or (js/isNaN hour) (js/isNaN minute))
-            (if (and (= challenge-hour hour) (= challenge-minute minute))
-                (set! (.-innerHTML (.getElementById js/document "body")) "<h1>Clue 9: 5</h1>")
-                (set! (.-value guess-input) "")))))
-
-(.addEventListener (.getElementById js/document "guess-button") "click" guess-fn)
-
-(defn print-answer [] (.log js/console (.concat "" challenge-hour ":" challenge-minute)))
+(js/setInterval draw-fn 1000)
